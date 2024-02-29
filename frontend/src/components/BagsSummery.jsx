a711fdf8d1a8609bf3cccaaa8fc274ed83e948d0
saurav-ghimire <sauravghimire0123@gmail.com>
2024-02-28 19:41:48 -0500

    cart pricing block create and fully functional

--- frontend/src/components/BagsSummery.jsx
+++ frontend/src/components/BagsSummery.jsx
@@ -1,35 +1,54 @@
+import { useSelector } from "react-redux";
+
+
 function BagsSummary({}) {
 
-const bagsSummary = {
-  totalItem: 3,
-  totalMRP: 232,
-  totalDiscount: 100,
-  finalPayment: 232
-}
+  const bagItemIds = useSelector((state) => state.bags);
+  
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItemIds.indexOf(item.id);
+    return itemIndex >= 0;
+  });
+
+  const CONVENIENCE_FEES = 99;
+  let totalItem = bagItemIds.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+
+  finalItems.forEach((bagItem) => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+
+  let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+
   return <>
-  <div className="bag-details-container">
-    <div className="price-header">PRICE DETAILS ({bagsSummary.totalItem} Items) </div>
-    <div className="price-item">
-      <span className="price-item-tag">Total MRP</span>
-      <span className="price-item-value">₹{bagsSummary.totalMRP}</span>
-    </div>
-    <div className="price-item">
-      <span className="price-item-tag">Discount on MRP</span>
-      <span className="price-item-value priceDetail-base-discount">-₹${bagsSummary.totalDiscount}</span>
-    </div>
-    <div className="price-item">
-      <span className="price-item-tag">Convenience Fee</span>
-      <span className="price-item-value">₹99</span>
-    </div>
-    <hr />
-    <div className="price-footer">
-      <span className="price-item-tag">Total Amount</span>
-      <span className="price-item-value">₹{bagsSummary.finalPayment}</span>
-    </div>
-  </div>
-  <button className="btn-place-order">
-    <div className="css-xjhrni">PLACE ORDER</div>
-  </button>
+      <div className="bag-details-container">
+        <div className="price-header">PRICE DETAILS ({totalItem} Items) </div>
+        <div className="price-item">
+          <span className="price-item-tag">Total MRP</span>
+          <span className="price-item-value">${totalMRP}</span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Discount on MRP</span>
+          <span className="price-item-value priceDetail-base-discount">
+            -${totalDiscount}
+          </span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Convenience Fee</span>
+          <span className="price-item-value">$99</span>
+        </div>
+        <hr />
+        <div className="price-footer">
+          <span className="price-item-tag">Total Amount</span>
+          <span className="price-item-value">${finalPayment}</span>
+        </div>
+      </div>
+      <button className="btn-place-order">
+        <div className="css-xjhrni">PLACE ORDER</div>
+      </button>
   </>;
 }
 
